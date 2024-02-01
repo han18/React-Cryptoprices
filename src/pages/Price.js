1b8d1c9d1c03378f5d050951210fb030a7a57e00
han18 <han.fadah@gmail.com>
2024-01-31 19:08:57 -0500

    adding the props

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -33,6 +33,9 @@ export default function Price(props) {
   const loaded = () => {
     return (
       <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
         <h2>{coin.rate}</h2>
       </div>
     );
