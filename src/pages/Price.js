e87203e398f5ef3e67465b67101de271a92d3d8e
han18 <han.fadah@gmail.com>
2024-01-31 19:07:23 -0500

    adding the props

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -10,13 +10,29 @@ export default function Price(props) {
   // Using the other two variables to create our URL.
   const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
 
+  // State to hold the coin data.
+  const [coin, setCoin] = useState("null");
+
+  // Function to fetch coin data.
+  const getCoin = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // useEffect to run getCoin when component mounts.
+  useEffect(() => {
+    getCoin();
+  }, []);
+
   // loaded function for when data is fetched.
   const loaded = () => {
     return (
       <div>
-        <h1>
-          {coin.asset_id_base}/{coin.asset_id_quote}
-        </h1>
         <h2>{coin.rate}</h2>
       </div>
     );
